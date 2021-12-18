d294b33c6ee8a16a5e6f0455e667d6407447da18
noboru-i <ishikura.noboru@gmail.com>
2021-12-18 22:40:36 +0900

    impl log out

--- lib/main.dart
+++ lib/main.dart
@@ -39,61 +39,46 @@ class AuthGate extends StatelessWidget {
         // User is not signed in
         if (!snapshot.hasData) {
           return const SignInScreen(
-              providerConfigs: [
-                EmailProviderConfiguration(),
-              ],
+            providerConfigs: [
+              EmailProviderConfiguration(),
+            ],
           );
         }
 
         // Render your application if authenticated
         return const MyHomePage(title: 'Flutter Demo Home Page');
       },
-    );  }
+    );
+  }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'email: ${FirebaseAuth.instance.currentUser?.email}',
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: const Text("Logout"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
