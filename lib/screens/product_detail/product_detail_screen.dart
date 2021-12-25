d185a2c0298e76a05ff11d62bfa5fb3998ff7daa
noboru-i <ishikura.noboru@gmail.com>
2021-12-26 01:05:00 +0900

    update product

--- lib/screens/product_detail/product_detail_screen.dart
+++ lib/screens/product_detail/product_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:beamer/beamer.dart';
 import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:price_memo/models/product.model.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -38,17 +40,93 @@ class _Body extends StatelessWidget {
 
         final snapshot = asyncSnapshot.data!;
         final product = snapshot.data;
+        final reference = snapshot.reference;
         if (product == null) {
           return const Text('error');
         }
 
-        return Column(
-          children: [
-            Text('name: ${product.name}'),
-            Text('latest price: ${product.latestPrice}'),
-          ],
-        );
+        return _DetailEditor(product, reference);
       },
+    );
+  }
+}
+
+class _DetailEditor extends HookConsumerWidget {
+  _DetailEditor(this.product, this.reference, {Key? key}) : super(key: key);
+
+  final Product product;
+  final ProductDocumentReference reference;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final productNameController = useTextEditingController(text: product.name);
+    final latestPriceController =
+        useTextEditingController(text: '${product.latestPrice}');
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: productNameController,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'product name',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text.';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: latestPriceController,
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'latest price',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text.';
+                }
+                if (int.tryParse(value) == null) {
+                  return 'Please enter numeric.';
+                }
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                if (_formKey.currentState?.validate() != true) {
+                  // has error.
+                  return;
+                }
+                await reference.update(
+                  name: productNameController.text,
+                  latestPrice: int.parse(latestPriceController.text),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Saved!'),
+                ));
+              },
+              child: const Text('Save'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
